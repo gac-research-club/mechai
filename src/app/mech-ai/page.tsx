7aed6665ee4114224702533770acef2c59dd16ec
Dimaspadma <Dimaspadma@users.noreply.github.com>
2023-08-30 13:47:35 +0700

    add priority play button

--- src/app/mech-ai/page.tsx
+++ src/app/mech-ai/page.tsx
@@ -19,7 +19,7 @@ export default function Page(){
         <div className="sm:flex sm:items-end pt-7">
           <h1 className="text-white text-5xl sm:text-7xl pe-5"><b>COMING SOON </b></h1>
           <button className="text-white pt-6 sm:pt-0" onClick={() => setShow(!show)}>
-            {show || <Image src="/play.png" width={64} height={64} alt="Play" quality={50} className="transition duration-300 ease-in-out hover:scale-110"/>}
+            {show || <Image src="/play.png" width={64} height={64} alt="Play" quality={50} className="transition duration-300 ease-in-out hover:scale-110" priority/>}
           </button>
         </div>
       </div>
